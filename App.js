a3ecd06779167423a261b97514594371f88584d8
canataa <vitorpinottti@gmail.com>
2025-05-14 11:51:54 +0000

    [feat]Add input text

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image,TextInput, Button, FlatList } from 'react-native';
 import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.viewLogo}>
       <Image source={logo} style={styles.logo}/>
-      <Text>Todo List</Text>
+      <Text style={styles.textLogo} >Todo List</Text>
+     <View style={styles.viewTask}>
+      <TextInput style={styles.inputTask} placeholder="Digite a tarefa"/>
+      <Button title="Adicionar"/>
+
+     </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,9 +25,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
   logo: {
     height: 200,
     width: 200,
+  },
+  viewTask:{
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  inputTask:{
+    flex: 1,
+    //height: 40,
+    //width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+   // marginBottom: 10,
+    paddingLeft: 10,
+    marginRight: 10,
+  },
+  viewLogo: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textLogo: {
+    justifyContent: "center",
   }
 });
